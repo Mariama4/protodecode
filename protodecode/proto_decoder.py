60c26309216a8e849f430f43c8b3b9a9c9ad83b4
georgijtaskabulov <mariama4@mail.ru>
2026-02-09 17:20:55 +0300

    bugfix while reading data

--- protodecode/proto_decoder.py
+++ protodecode/proto_decoder.py
@@ -1,9 +1,11 @@
-import struct
-from .types import TYPES
-from .utils import parse_input_data, buffer_to_pretty_hex
-from .varint import decode_varint, decode_varint_parts
-from .fixed import decode_fixed32, decode_fixed64
 from .buffer_reader import BufferReader
+from .fixed import decode_fixed32, decode_fixed64
+from .types import TYPES
+from .utils import buffer_to_pretty_hex, parse_input_data
+from .varint import decode_varint_parts
+
+MAX_REASONABLE_FIELD_INDEX = 1_000_000
+
 
 def decode_proto(buffer: bytes):
     reader = BufferReader(buffer)
@@ -18,35 +20,36 @@ def decode_proto(buffer: bytes):
             type_field = index_type & 0b111
             index = index_type >> 3
 
-            if type_field == TYPES['VARINT']:
+            if type_field == TYPES["VARINT"]:
                 value = str(reader.read_varint())
-            elif type_field == TYPES['LENDELIM']:
+            elif type_field == TYPES["LENDELIM"]:
                 length = int(reader.read_varint())
                 value = reader.read_buffer(length)
-            elif type_field == TYPES['FIXED32']:
+            elif type_field == TYPES["FIXED32"]:
                 value = reader.read_buffer(4)
-            elif type_field == TYPES['FIXED64']:
+            elif type_field == TYPES["FIXED64"]:
                 value = reader.read_buffer(8)
             else:
                 raise Exception("Unknown type: " + str(type_field))
             byte_range.append(reader.offset)
 
-            parts.append({
-                "byteRange": byte_range,
-                "index": index,
-                "type": type_field,
-                "value": value
-            })
+            parts.append(
+                {
+                    "byteRange": byte_range,
+                    "index": index,
+                    "type": type_field,
+                    "value": value,
+                }
+            )
     except Exception:
         reader.reset_to_checkpoint()
 
-    return {
-        "parts": parts,
-        "leftOver": reader.read_buffer(reader.left_bytes())
-    }
+    return {"parts": parts, "leftOver": reader.read_buffer(reader.left_bytes())}
+
 
 def ProtobufVarintPart(value):
     return decode_varint_parts(value)
+
 
 def ProtobufFixed32Part(value: bytes):
     decoded = decode_fixed32(value)
@@ -58,38 +61,54 @@ def ProtobufFixed32Part(value: bytes):
             return float_val
     return decoded
 
+
 def ProtobufFixed64Part(value: bytes):
     return decode_fixed64(value)
+
 
 def ProtobufStringOrBytesPart(value):
     return value["value"]
 
+
+def is_reasonable_nested_protobuf(decoded):
+    if len(decoded["leftOver"]) != 0 or not decoded["parts"]:
+        return False
+    for part in decoded["parts"]:
+        if part["index"] <= 0 or part["index"] > MAX_REASONABLE_FIELD_INDEX:
+            return False
+    return True
+
+
 def get_protobuf_part(part):
-    if part["type"] == TYPES['VARINT']:
+    if part["type"] == TYPES["VARINT"]:
         return (ProtobufVarintPart(part["value"]), None)
-    elif part["type"] == TYPES['LENDELIM']:
+    elif part["type"] == TYPES["LENDELIM"]:
         decoded = decode_proto(part["value"])
-        if len(part["value"]) > 0 and len(decoded["leftOver"]) == 0 and decoded["parts"]:
+        if is_reasonable_nested_protobuf(decoded):
             return (ProtobufDisplay(decoded), "protobuf")
         else:
             decoded2 = decode_string_or_bytes(part["value"])
             return (ProtobufStringOrBytesPart(decoded2), decoded2["type"])
-    elif part["type"] == TYPES['FIXED64']:
+    elif part["type"] == TYPES["FIXED64"]:
         return (ProtobufFixed64Part(part["value"]), None)
-    elif part["type"] == TYPES['FIXED32']:
+    elif part["type"] == TYPES["FIXED32"]:
         return (ProtobufFixed32Part(part["value"]), None)
     else:
         return ("Unknown type", None)
+
 
 def ProtobufPart(part):
     contents, _ = get_protobuf_part(part)
     return contents
 
+
 def ProtobufDisplay(decoded):
     return [ProtobufPart(part) for part in decoded["parts"]]
 
+
 def decode_grpc(data: str):
     return ProtobufDisplay(decode_proto(parse_input_data(data)))
+
 
 def decode_string_or_bytes(value: bytes):
     if len(value) == 0:
